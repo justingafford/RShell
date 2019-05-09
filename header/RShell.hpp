9c04679a88a00f7d9c14c5c3159386527108325f
Justin Gafford <jgaff001@ucr.edu>
2019-05-09 09:08:46 -0700

    Defining header files with their descriptions in the UML Diagrams

--- header/RShell.hpp
+++ header/RShell.hpp
@@ -1,1 +1,19 @@
+#ifndef RSHELL_HPP
+#define RSHELL_HPP
+#include <iostream>
+using namespace std;
 
+class RShell {
+  private:
+   //sting userCommand;
+   void printDollarSign() {
+     //infinite loop running program
+     //while(1) {
+       cout << "$";
+       //cin >> userCommand;
+       //perform neccessary 
+       //cout << endl;
+     //}
+   };
+};
+#endif
