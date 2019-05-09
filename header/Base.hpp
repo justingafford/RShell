ed29606088bf92f76c2ce462d81f983cda7eb4aa
Justin Gafford <jgaff001@ucr.edu>
2019-05-09 08:53:26 -0700

    Defining header files with their descriptions in the UML Diagrams

--- header/Base.hpp
+++ header/Base.hpp
@@ -1,1 +1,18 @@
+#ifndef BASE_HPP
+#define AMPERSAND_HPP
+#include "Connectors.hpp"
+using namespace std;
 
+class Base {
+ public:
+  Base* lhs;
+  Base* rhs;
+  Base() {
+    lhs = 0;
+    rhs = 0;
+  };
+ private:
+  virtual void evaluate() = 0;
+  
+};
+#endif
