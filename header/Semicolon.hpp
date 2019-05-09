665c20805d8f990c88ff00bcef957dd8120a8dce
Justin Gafford <jgaff001@ucr.edu>
2019-05-09 09:13:22 -0700

    Defining header files with their descriptions in the UML Diagrams

--- header/Semicolon.hpp
+++ header/Semicolon.hpp
@@ -1,1 +1,19 @@
+#ifndef SEMICOLON_HPP
+#define SEMICOLON_HPP
+#include "Connectors.hpp"
+using namespace std;
 
+class Semicolon:: public Connectors {
+ public:
+  Base* lhs;
+  Base* rhs;
+ private:
+  Semicolon () { 
+    lhs = 0;
+    rhs = 0;
+  };
+  Semicolon(Base* left,Base* right);
+  void evaluate();
+  
+};
+#endif
