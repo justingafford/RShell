98814b8197a816cb3f35651f75d887728ec0ef33
Justin Gafford <jgaff001@ucr.edu>
2019-05-09 08:49:32 -0700

    Defining header files with their descriptions in the UML Diagrams

--- header/Ampersand.hpp
+++ header/Ampersand.hpp
@@ -1,1 +1,14 @@
+#ifndef AMPERSAND_HPP
+#define AMPERSAND_HPP
+#include "Connectors.hpp"
+using namespace std;
 
+class Ampersand:: public Connectors {
+ public:
+  Base* lhs;
+  Base* rhs;
+ private:
+  void evaluate();
+  
+};
+#endif
