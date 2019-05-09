5bbfcaadcd58030a1387fcabe29e9c5125b6c56b
Justin Gafford <jgaff001@ucr.edu>
2019-05-09 09:00:41 -0700

    Defining header files with their descriptions in the UML Diagrams

--- header/Connectors.hpp
+++ header/Connectors.hpp
@@ -1,1 +1,14 @@
+#ifndef CONNECTORS_HPP
+#define CONNECTORS_HPP
+#include "Base.hpp"
+using namespace std;
 
+class Connectors: class Base {
+ public:
+  Base* lhs;
+  Base* rhs;
+ private:
+  void evaluate();
+  
+};
+#endif
