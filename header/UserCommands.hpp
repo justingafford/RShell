3f7f8137109fbc91ccfaeeaffe379247818fd5cd
Justin Gafford <jgaff001@ucr.edu>
2019-05-09 09:44:11 -0700

    Defining header files with their descriptions in the UML Diagrams

--- header/UserCommands.hpp
+++ header/UserCommands.hpp
@@ -1,1 +1,26 @@
+#ifndef USERCOMMANDS_HPP
+#define USERCOMMANDS_HPP
+#include "Base.hpp"
+#include <iostream>
+#include <queue> //use a queue to hold usercommands
+using namespace std;
 
+class UserCommands:: public Base {
+ private:
+  queue<Base*> userCommands;
+ public:
+  UserCommands () {
+    userCommands = 0;
+  };
+  //call the destructor whenever we finish user commands(i.e. they press enter, and it compiles)
+  ~UserCommands () {
+    queue<Base*> empty;
+    userCommands = empty;
+  };
+  UserCommands(Base* comm) {
+    userCommands.push(comm);
+  };
+  void evaluate();
+  
+};
+#endif
