1fae587f2b9f5623a128f0e6ad1053a6aad4c6a4
Justin Gafford <jgaff001@ucr.edu>
2019-05-09 09:01:29 -0700

    Defining header files with their descriptions in the UML Diagrams

--- header/Pipe.hpp
+++ header/Pipe.hpp
@@ -1,1 +1,14 @@
+#ifndef PIPE_HPP
+#define PIPE_HPP
+#include "Connectors.hpp"
+using namespace std;
 
+class Pipe:: public Connectors {
+ public:
+  Base* lhs;
+  Base* rhs;
+ private:
+  void evaluate();
+  
+};
+#endif
