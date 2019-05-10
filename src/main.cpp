5159e3c1fe015bd640c4f7f886a4bf24a112852c
Justin Gafford <jgaff001@ucr.edu>
2019-05-09 20:12:58 -0700

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "Ampersand.hpp"
+#include "Base.hpp"
+#include "Connectors.hpp"
+#include "Pipe.hpp"
+#include "RShell.hpp"
+#include "Semicolon.hpp"
+#include "UserCommands.hpp"
+
+void main () {
+
+  while (1) {
+    
+  }
+}
