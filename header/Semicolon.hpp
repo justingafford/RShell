12d53a0f7c39b2ebefe04634c39b14da394d3571
Justin Gafford <jgaff001@ucr.edu>
2019-05-11 04:42:37 -0700

    Update Semicolon.hpp

--- header/Semicolon.hpp
+++ header/Semicolon.hpp
@@ -1,19 +1,20 @@
 #ifndef SEMICOLON_HPP
 #define SEMICOLON_HPP
 #include "Connectors.hpp"
+#include "UserCommands.hpp"
 using namespace std;
 
 class Semicolon : public Connectors {
  private:
-  Base* lhs;
-  Base* rhs;
+  UserCommands* lhs;
+  UserCommands* rhs;
  public:
   Semicolon () { 
     lhs = 0;
     rhs = 0;
   };
-  Semicolon(Base* left,Base* right);
-  void evaluate();
+  Semicolon(UserCommands* left,UserCommands* right);
+  bool evaluate();
   
 };
 #endif
