fc6d5c6ea444ede2288bb8d61a932ddaab9162a4
Justin Gafford <jgaff001@ucr.edu>
2019-05-11 05:35:03 -0700

    Update Pipe.cpp

--- src/Pipe.cpp
+++ src/Pipe.cpp
@@ -1,1 +1,17 @@
+ #include "Pipe.hpp"
+ #include <iostream>
+ 
+using namespace std;
 
+Pipe::Pipe(UserCommands* left, UserCommands* right) {
+   lhs = left;
+   rhs = right;
+ }
+void Pipe::evaluate() {
+   if (!lhs->ExecuteStatus()) {
+     rhs->DoExecute();
+   }
+   else {
+     rhs->DoNotExecute();
+   }
+}
