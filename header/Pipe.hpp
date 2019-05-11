bff06219a6b7a80cb7e6c2fe7a3924fed2fc7f79
Justin Gafford <jgaff001@ucr.edu>
2019-05-11 04:49:00 -0700

    changed base pointer to type UserCommands

--- header/Pipe.hpp
+++ header/Pipe.hpp
@@ -1,19 +1,19 @@
 #ifndef PIPE_HPP
 #define PIPE_HPP
 #include "Connectors.hpp"
-#include "UserCommand.hpp"
+#include "UserCommands.hpp"
 using namespace std;
 
 class Pipe : public Connectors {
  private:
-  UserCommand* lhs;
-  UserCommand* rhs;
+  UserCommands* lhs;
+  UserCommands* rhs;
  public:
   Pipe() {
    lhs = 0;
    rhs = 0;
   };
-  Pipe(UserCommands* left, UserCommand* right);
+  Pipe(UserCommands* left, UserCommands* right);
   void evaluate();
   
 };
