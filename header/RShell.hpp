2fd0a2349257420999c152d3623cc8f3979ed881
Justin Gafford <jgaff001@ucr.edu>
2019-05-10 23:55:34 -0700

    Update RShell.hpp

--- header/RShell.hpp
+++ header/RShell.hpp
@@ -1,6 +1,12 @@
 #ifndef RSHELL_HPP
 #define RSHELL_HPP
 #include <iostream>
+#include "Base.hpp"
+#include "UserCommands.hpp"
+#include "Connectors.hpp"
+#include "Ampersand.hpp"
+#include "Pipe.hpp"
+#include "Semicolon"
 using namespace std;
 
 class RShell {
