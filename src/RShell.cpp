7d0cea4f45a7f27785f0b8620e466ec4e7fd3f61
Justin Gafford <jgaff001@ucr.edu>
2019-05-11 02:32:12 -0700

    Update RShell.cpp

--- src/RShell.cpp
+++ src/RShell.cpp
@@ -1,10 +1,11 @@
+#include "RShell.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 void RShell::program () {
-  while(userInput != exit) { //
-    printDollarSign();
-    //ask for user input
-    //user inputs line i.e. ls -a; echo hello && mkdir test || echo world; git status
-    //create a tree
-    //execute command line
-  }
+
 }
 
