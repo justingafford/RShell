f92c5fa61d86e79e6c3bd827eb55ef239287ba05
Wayland Chang <32940536+wchan051@users.noreply.github.com>
2019-05-12 13:44:35 -0700

    Update multiple_commands_tests.sh

--- integration_tests/multiple_commands_tests.sh
+++ integration_tests/multiple_commands_tests.sh
@@ -1,1 +1,7 @@
-
+ls -a; echo hello && mkdir test || echo world; git status
+ls -a || ls
+ls -a && ls
+echo "hello" && rm -rf test
+mkdir testagain && echo successfully made testagain directory
+ls; rm -rf testagain; ls
+exit
