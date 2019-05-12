b0276ed75ccee776b4d1b41f2f9c528727398d37
Justin Gafford <jgaff001@ucr.edu>
2019-05-11 20:28:38 -0700

    Update RShell.cpp

--- src/RShell.cpp
+++ src/RShell.cpp
@@ -14,8 +14,8 @@ void RShell::parse () {
     //parse part 1(put commands into vector of strings)
     for(unsigned i = 0; i < terminalCommand.size();  i++) {
     
-    while(terminalCommand[i] != ";" || terminalCommand[i] !="&"  || terminalCommand[i]!="|" || terminalCommand[i]!="#" || terminalCommand[i] = "/n" ) {
-	ss << terminalCommand[i]; 
+    while(terminalCommand.at(i) != ";" || terminalCommand.at(i) !="&"  || terminalCommand.at(i)!="|" || terminalCommand.at(i)!="#" || terminalCommand.at(i) = "/n" ) {
+	ss << terminalCommand.at(i); 
 	ss >> currFunction; 
 	i++;
     }
@@ -27,57 +27,57 @@ void RShell::parse () {
 // and Connectors
 unsigned save = -1;
 for (unsigned i = 0; i < parsed.size(); i++) {
-    if(parsed[i] == ";") {
+    if(parsed.at(i) == ";") {
         if(i > 0 && i < parsed.size()) {
-	    Semicolon semi = new Semicolon(parsed[i-1],parsed[i+1]);
+	    Semicolon semi = new Semicolon(parsed.at(i-1),parsed.at(i+1));
 	    else if (i > 1 && i == parsed.size()) {
-		Semicolon* semi = new Semicolon(parsed[i-1],"");
+		Semicolon* semi = new Semicolon(parsed.at(i-1),"");
 	    }
 	    else if (parsed.size() ==1) {
    		Semicolon* semi = new Semicolon();
 	    }
-	input.push_back[semi];
+	input.push_back(semi);
     }
-    else if(parsed[i] == "||") {
+    else if(parsed.at(i) == "||") {
         if(i > 0 && i < parsed.size()) {
-	    Pipe* pip = new Pipe(parsed[i-1],parsed[i+1]);
+	    Pipe* pip = new Pipe(parsed.at(i-1),parsed.at(i+1));
 	    else if (i > 1 && i == parsed.size()) {
-		    Pipe* pip= new Pipe(parsed[i-1],"");
+		    Pipe* pip= new Pipe(parsed.at(i-1),"");
 	    }
 	    else if (parsed.size() ==1) {
    		Pipe* pip= new Pipe();
 	    }
-	    input.push_back[pip];
+	    input.push_back(pip);
         }
         else if (parsed[i] == "&&") {
             if(i > 0 && i < parsed.size()) {
-	        Ampersand amp = new Ampersand(parsed[i-1],parsed[i+1]);
+	        Ampersand amp = new Ampersand(parsed.at(i-1),parsed.at(i+1));
 	        else if (i > 1 && i == parsed.size()) {
-		     Ampersand* amp = new Ampersand(parsed[i-1],"");
+		     Ampersand* amp = new Ampersand(parsed.at(i-1),"");
 	        }
 	        else if (parsed.size() ==1) {
    		     Ampersand* amp = new Ampersand();
 	        }
-	        input.push_back[amp];
+	        input.push_back(amp);
         }
         else if (parsed[i] = = "#") {
 	    save = i;
 	    break;
         }
         else if (parsed.size() != 0) {
-	    UserCommands* comm = new UserCommands[parsed[i]];
-	    input.push_back[comm];
+	    UserCommands* comm = new UserCommands[parsed.at(i)];
+	    input.push_back(comm);
         }
     }
 //part 3
-    if(saved != -1) {
+    if(save != -1) {
         ofstream ofs;	
         ofs.open("comments.txt");
         if(ofs.is_open()) {
-            for(unsigned j = parsed[save]; j < parsed.size();j++) {
+            for(unsigned j = parsed.at(save); j < parsed.size();j++) {
 	
                 //write parsed[j] to comment section;
-                ofs << parsed[j];
+                ofs << parsed.at(j);
             }
             ofs.close();	
         }
