479210443a363ec212bc2235971917b6f0efd9ab
Justin Gafford <jgaff001@ucr.edu>
2019-05-12 14:58:42 -0700

    Update Base.hpp

--- header/Base.hpp
+++ header/Base.hpp
@@ -6,12 +6,17 @@ class Base {
  protected:
   Base* lhs;
   Base* rhs;
+  char* argument[256]
  public:
   Base() {
     lhs = 0;
     rhs = 0;
   };
+  virtual string returnCommand();
+  virtual bool DoExecute();
+  virtual bool DoNotExecute();
   virtual void evaluate() = 0;
+  virtual bool evaluate();
  
 };
 #endif
