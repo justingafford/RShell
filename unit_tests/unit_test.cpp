63bca5c29befd13291d181cb194e515441241310
Justin Gafford <jgaff001@ucr.edu>
2019-05-11 21:14:42 -0700

    Update unit_test.cpp

--- unit_tests/unit_test.cpp
+++ unit_tests/unit_test.cpp
@@ -1,1 +1,16 @@
+#include "../header/Ampersand.hpp"
+#include "../header/Base.hpp"
+#include "../header/Connectors.hpp"
+#include "../header/Pipe.hpp"
+#include "../header/RShell.hpp"
+#include "../header/Semicolon.hpp"
+#include "../header/UserCommands.hpp"
+#include #include "gtest/gtest.h"
+#include <iostream>
 
+using namespace std;
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
