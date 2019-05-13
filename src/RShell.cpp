87216ce6629cbe94279b0cef966641e715aac21e
Justin Gafford <jgaff001@ucr.edu>
2019-05-12 17:28:06 -0700

    Update RShell.cpp

--- src/RShell.cpp
+++ src/RShell.cpp
@@ -15,136 +15,84 @@ void RShell::parse () {
     for(unsigned i = 0; i < terminalCommand.size();  i++) {
     
     	while(terminalCommand.at(i) != ';'|| terminalCommand.at(i) !='&'  || terminalCommand.at(i)!='|' || terminalCommand.at(i)!='#' || terminalCommand.at(i) != '/n' ) {
-	    ss << terminalCommand.at(i); 
-	    ss >> currFunction; 
-	    i++;
+	    	ss << terminalCommand.at(i); 
+	    	ss >> currFunction; 
+	    	i++;
     	}
 	
-	if(terminalCommand.at(i) == ';') {
-	    UserCommands* temp = new UserCommands;
-	    temp->setCommand(";");
+		if(terminalCommand.at(i) == ';') {
+	    	UserCommands* temp = new UserCommands;
+	    	temp->setCommand(";");
     	    parsed.push_back(temp);
-	}
-	else if(terminalCommand.at(i) == '|') {
-	    UserCommands* temp = new UserCommands;
-	    temp->setCommand("||");
+		}
+		else if(terminalCommand.at(i) == '|') {
+	    	UserCommands* temp = new UserCommands;
+	    	temp->setCommand("||");
     	    parsed.push_back(temp);
-	}
-	else if(terminalCommand.at(i) == '&') {
-	    UserCommands* temp = new UserCommands;
-	    temp->setCommand("&&");
+		}
+		else if(terminalCommand.at(i) == '&') {
+	    	UserCommands* temp = new UserCommands;
+	    	temp->setCommand("&&");
+    		parsed.push_back(temp);
+		}
+		else if(terminalCommand.at(i) == '#') {
+	    	UserCommands* temp = new UserCommands;
+	    	temp->setCommand("#");
     	    parsed.push_back(temp);
-	}
+		}
     	else if(terminalCommand.at(i) != '\n') {
-	    UserCommands* temp = new UserCommands;
-	    temp->setCommand(currFunction);
-    	    parsed.push_back(temp);
-   	}
+	    	UserCommands* temp = new UserCommands;
+	    	temp->setCommand(currFunction);
+    		parsed.push_back(temp);
+   		}
     }
 
     //parse part 2(this populates input vector by allocating new variables of type UserCommands
     // and Connectors
     unsigned save = -1;
-    bool zeroset = false;
-    if (parsed.size() > 1) {
-        for (unsigned i = 1; i < (parsed.size() - 1); i++) {
-		
-	    if(parsed.at(i)->returnCommand() == ";") {
-        	if(i < (parsed.size() - 1) && zeroset) {
-	    	    Semicolon* semi = new Semicolon(parsed.at(i-1),parsed.at(i+1));
-		    input.push_back(semi);
-		}
-		else if (i == (parsed.size() - 1) && zeroset) {
-		    UserCommands* cmd = new UserCommands();
-		    Semicolon* semi = new Semicolon(parsed.at(i-1),cmd);
-		    input.push_back(semi);
-		}
-		else if(parsed.at(0)->returnCommand() == ";" && !zeroset) {
-		    UserCommands* cmd = new UserCommands();
-		    Semicolon* semi= new Semicolon(cmd,parsed.at(1));
-		    input.push_back(semi);
-		    zeroset = true;
-		}
-			
-	    }
-		
-	    else if(parsed.at(i)->returnCommand() == "||") {
-        	if(i < (parsed.size() -1) && zeroset) {
-	            Pipe* pip = new Pipe(parsed.at(i-1),parsed.at(i+1));
-		    input.push_back(pip);
-		}
-	    	else if (i > 1 && i == (parsed.size() - 1) && zeroset) {
-		    UserCommands* cmd = new UserCommands();
-		    Pipe* pip= new Pipe(parsed.at(i-1),cmd);
-		    input.push_back(pip);
+    for (unsigned i = 0; i < parsed.size(); i++) {
+		if(parsed.size() > 2) {
+			if(parsed.at(i)->returnCommand() == ";") {
+	        	Semicolon* semi = new Semicolon(parsed.at(i-1),parsed.at(i+1));
+				input.push_back(semi);
 	    	}
-	    	else if(parsed.at(0)->returnCommand() == "&&" && !zeroset) {
-		    UserCommands* cmd = new UserCommands();
-		    Pipe* pip= new Pipe(cmd,parsed.at(1));
-		    input.push_back(pip);
-		    zeroset = true;
-				
-		}
-	    
+			else if(parsed.at(i)->returnCommand() == "||") {
+        		Pipe* pip = new Pipe(parsed.at(i-1),parsed.at(i+1));
+		    	input.push_back(pip);
+			}
+			else if (parsed.at(i)->returnCommand() == "&&") {
+            	Ampersand* amp = new Ampersand(parsed.at(i-1),parsed.at(i+1));
+				input.push_back(amp);
+			}
+			else if (parsed.at(i)->returnCommand() == "#") {
+	    		save = i;
+	    		break;
+        	}
+			else {
+	    		UserCommands* comm = new UserCommands();
+				comm = parsed.at(i);
+	    		input.push_back(comm);
+			}    
 	    }
-		
-            else if (parsed.at(i)->returnCommand() == "&&") {
-            	if(i < (parsed.size() - 1) && zeroset) {
-	            Ampersand* amp = new Ampersand(parsed.at(i-1),parsed.at(i+1));
-		    input.push_back(amp);
+		else {
+			if(parsed.at(i)->returnCommand() == ";" || parsed.at(i)->returnCommand() == "||" || parsed.at(i)->returnCommand() == "&&") {
+				cout << "Error: connectors(';','||','&&') must have a left and right operand. << endl;
+			}
+			else {
+				if(parsed.at(i)->returnCommand() == "#") {
+					save = i;
+					break;
+				}
+				else { //parsed.at(i) is a UserCommand
+					UserCommands* comm = new UserCommands();
+					comm = parsed.at(i);
+	    			input.push_back(comm);
+				}
+			}
 		}
-	        else if (i > 1 && i == (parsed.size() -1) && zeroset) {
-		    UserCommands* cmd = new UserCommands();
-		    Ampersand* amp = new Ampersand(parsed.at(i-1),cmd);
-		    input.push_back(amp);
-	        }
-	        	
-	        else if(parsed.at(0)->returnCommand() == "&&" && !zeroset) {
-		    UserCommands* cmd = new UserCommands();
-		    Ampersand* amp= new Ampersand(cmd,parsed.at(1));
-		    input.push_back(amp);
-		    zeroset = true;
-		}
-	    }
-			
-            else if (parsed.at(i)->returnCommand() == "#") {
-	    	save = i;
-	    	break;
-            }
-			
-            else {
-	    	UserCommands* comm = new UserCommands();
-		comm = parsed.at(i);
-	    	input.push_back(comm);
-	        if(!zeroset) {
-		    comm = parsed.at(i);
-	    	    input.push_back(comm);
-		    zeroset = true;
-		    }
-                }
-	    }
-        }
-    else if(parsed.size() == 1) {
-	if (parsed.at(0)->returnCommand() == "#") {
-	    save = 0;
-        }
-	else if (parsed.at(0)->returnCommand() == ";") {
-	    Semicolon* semi= new Semicolon();
-	    input.push_back(semi);
-	}
-	else if(parsed.at(0)->returnCommand() == "||") {
-	    Pipe* pip= new Pipe();
-	    input.push_back(pip);
-	}
-	else if (parsed.at(0)->returnCommand() == "&&") {
-	    Ampersand* amp= new Ampersand();
-	    input.push_back(amp);
-	}
-	else {
-	    UserCommands* comm = new UserCommands();
-	    comm = parsed.at(0);
-	    input.push_back(comm);
-	}
+					
+  	}
+    
     }
 	//part 3
     if(save != -1) {
