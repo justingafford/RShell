06208c8c523c6f2a397a1de2274591e3810a3f05
Justin Gafford <jgaff001@ucr.edu>
2019-05-14 17:25:13 -0700

    Update RShell.cpp

--- src/RShell.cpp
+++ src/RShell.cpp
@@ -17,7 +17,7 @@ void RShell::parse () {
 	while(terminalCommand.at(i) != ';'|| terminalCommand.at(i) !='&'  || terminalCommand.at(i)!='|' || terminalCommand.at(i)!='#' || terminalCommand.at(i) != '\n' ) {
 		cout << "while" << endl;
 		if(i < terminalCommand.size()) {
-			getline(ss,terminalCommand.at(i));
+			ss >> noskipws >>terminalCommand.at(i);
 			i++;
 			if (i == (terminalCommand.size() - 1)) {
 	 			break;
