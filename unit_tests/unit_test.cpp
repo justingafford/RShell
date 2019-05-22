a462b0ad99d4303a8216dc65529a021b2f870497
Justin Gafford <jgaff001@ucr.edu>
2019-05-21 19:11:45 -0700

    Update unit_test.cpp

--- unit_tests/unit_test.cpp
+++ unit_tests/unit_test.cpp
@@ -80,8 +80,8 @@ TEST(RShellTests, MultipleCommandTest) {
     test.parse();
     test.program();
     ASSERT_EQ(test.accessParsed().at(0)->returnCommand(),"echo hello ");
-    ASSERT_EQ(test.accessParsed().at(1)->returnCommand(),"&&");
-    ASSERT_EQ(test.accessParsed().at(2)->returnCommand()," echo world");
+    ASSERT_EQ(test.accessParsed2().at(0)->returnCommand(),"&&");
+    ASSERT_EQ(test.accessParsed().at(1)->returnCommand()," echo world");
     ASSERT_EQ(test.accessParsed().size(),3);
     test.reset();
     ASSERT_EQ(test.accessParsed().empty(),true);
