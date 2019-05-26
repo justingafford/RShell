27e6ffe5aa53da6d9fb5f0993181a7f47b2bd8a4
Justin Gafford <jgaff001@ucr.edu>
2019-05-26 16:10:32 -0700

    Update test_literal_tests.sh

--- integration_tests/test_literal_tests.sh
+++ integration_tests/test_literal_tests.sh
@@ -1,1 +1,7 @@
-
+#!/bin/sh
+test -e test/file/path && echo “path exists”
+test -e test/Ampersand.cpp/src
+test test/Ampersand.hpp/header
+test -e test/RShell.hpp/headerr
+test -f test/Ampersand.hpp/header
+test -d test/Ampersand.hpp/header
