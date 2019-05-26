c82317f8cb88a579da11174a4a6dc24e0e374d8b
Justin Gafford <jgaff001@ucr.edu>
2019-05-26 16:07:30 -0700

    Update precedence_tests.sh

--- integration_tests/precedence_tests.sh
+++ integration_tests/precedence_tests.sh
@@ -1,1 +1,7 @@
-
+#!/bin/sh
+(echo A && echo B) || (echo C && echo D)
+(yeet || echo hello) && (mkdir test ; ls)
+this || (echo hello || echo friend) 
+(ls -a ; git status) || (echo a || echo b) && (mkdir finish && ls)
+(echo A) && (echo B) && (echo C)
+(echo this should work)
