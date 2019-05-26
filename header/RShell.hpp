e29e0704fa652f6d9e2b48f1668367eb2d8e9f10
Justin Gafford <jgaff001@ucr.edu>
2019-05-25 17:40:27 -0700

    Update RShell.hpp

--- header/RShell.hpp
+++ header/RShell.hpp
@@ -27,14 +27,6 @@ class RShell {
     vector<Base*> accessInput () {
         return input;
     };
-    void createCommands(vector<UserCommands*> parsedd){
-       char * temp = new char[terminalCommand.length() + 1];             
-       strcpy(temp, terminalCommand.c_str());                                  
-       char * token = strtok(temp, " ;|&#"); 
-       while (token != NULL) {                                                 
-            argument.push_back(yeet);
-            token = strtok(NULL, " ;|&#");
-      }
     };
     bool commentCreated () {
         return createdComment;
