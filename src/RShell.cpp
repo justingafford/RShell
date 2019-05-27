b89f3bc1d3ed447d67216b20e61ee88413e41687
Justin Gafford <jgaff001@ucr.edu>
2019-05-26 21:10:19 -0700

    Update RShell.cpp

--- src/RShell.cpp
+++ src/RShell.cpp
@@ -209,7 +209,7 @@ void RShell::program() {
       if(parsed.at(i)->ExecuteStatus()) {
 	parsed.at(i)->DoNotExecute();
 	pid_t pid = fork();
-	while(wait(0) != pid);
+	wait(0);
 	//pid_t pid = 0;
 	if(pid < 0) {
 	  perror("Fork() failed.");
